4b7d0f2ffaf502f740852fd9483a80cd1c9295cd
ylieumo <yoldjampou@gmail.com>
2024-01-18 15:35:33 -0500

    update

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ app.get("/api/users", (req, res) => {
 // using a route parameter for the unique id.
 app.get("/api/users/:id", (req, res) => {
     const user = users.find((u) => u.id == req.params.id);
-    if (user) res.json(user);
+    if (user) {res.json(user)
+    } else {
+  res.send("user not found")}
   });
-  if (user) {res.json(user)
-  } else {
-res.send("user not found")}
+
 
 // index route for posts
 // Creating a GET route for the entire posts database.
@@ -37,11 +37,11 @@ app.get("/api/posts", (req, res) => {
 // using a route parameter for the unique id.
 app.get("/api/posts/:id", (req, res) => {
     const post = posts.find((p) => p.id == req.params.id);
-    if (post) res.json(post);
+    if (post) {res.json(post)
+    } else {
+  res.send("post not found")}
   });
-  if (post) {res.json(post)
-  } else {
-res.send("post not found")}
+  
 
 app.get("/", (req, res) => {
     res.send("All usable route start in slash api!");
