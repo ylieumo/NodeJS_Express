1be215083467dab5d3a47d261a635d2fcc6be366
ylieumo <yoldjampou@gmail.com>
2024-01-19 11:27:50 -0500

    added error handleling with utilities with error fumction

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const port = 3000
 const users = require("./data/users");
 const posts = require("./data/posts");
 
+const error = require("./utilities/error");
+
 //  all routes 
 // We use the body-parser middleware FIRST so that
 // we have access to the parsed data within our routes.
@@ -144,10 +146,83 @@ app.get("/", (req, res) => {
   res.send("Work in progress!");
 });
 
+
+// Parsing Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
+
+// Logging Middlewaare
+app.use((req, res, next) => {
+  const time = new Date();
+
+  console.log(
+    `-----
+${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
+  );
+  if (Object.keys(req.body).length > 0) {
+    console.log("Containing the data:");
+    console.log(`${JSON.stringify(req.body)}`);
+  }
+  next();
+});
+
+// Valid API Keys.
+apiKeys = ["perscholas", "ps-example", "hJAsknw-L198sAJD-l3kasx"];
+
+// New middleware to check for API keys!
+// Note that if the key is not verified,
+// we do not call next(); this is the end.
+// This is why we attached the /api/ prefix
+// to our routing at the beginning!
+app.use("/api", function (req, res, next) {
+  var key = req.query["api-key"];
+
+  // Check for the absence of a key.
+  if (!key) {
+    res.status(400);
+    return res.json({ error: "API Key Required" });
+  }
+
+  // Check for key validity.
+  if (apiKeys.indexOf(key) === -1) {
+    res.status(401);
+    return res.json({ error: "Invalid API Key" });
+  }
+
+  // Valid key! Store it in req.key for route access.
+  req.key = key;
+  next();
+});
+
+// // Use our Routes
+// app.use("/api/users", users);
+// app.use("/api/posts", posts);
+
+// app.get("/", (req, res) => {
+//   res.send("Work in progress!");
+// });
+
+// 404 Middleware
 app.use((req, res) => {
   res.status(404);
   res.json({ error: "Resource Not Found" });
 });
+
+
+// Error-handling middleware.
+// Any call to next() that includes an
+// Error() will skip regular middleware and
+// only be processed by error-handling middleware.
+// This changes our error handling throughout the application,
+// but allows us to change the processing of ALL errors
+// at once in a single location, which is important for
+// scalability and maintainability.
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({ error: err.message });
+  });
+
+
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}.`);
---
+++ utilities/error.js
@@ -0,0 +1,8 @@
+function error(status, msg) {
+    var err = new Error(msg);
+    err.status = status;
+    return err;
+  }
+  
+  module.exports = error;
+  
